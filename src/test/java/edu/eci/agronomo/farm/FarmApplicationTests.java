1eda1e25af95273ee7e9819335ea10b09ce6a726
diego <diegofernandoruizrojas@gmail.com>
2023-03-01 09:20:09 -0500

    Innitial Structure

---
+++ src/test/java/edu/eci/agronomo/farm/FarmApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.eci.agronomo.farm;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FarmApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
